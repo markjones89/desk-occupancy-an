e9731c2978296dc4e0676f2aba1cfdf184a6a6a2
jhgjeraker <jhgjeraker@protonmail.com>
2020-08-18 11:11:21 +0200

    reset auth details

--- sensor_stream.py
+++ sensor_stream.py
@@ -2,9 +2,9 @@
 from occupancy.director import Director
 
 # Fill in from the Service Account and Project:
-USERNAME   = "brn1e6r24te000b24bp0"             # this is the key
-PASSWORD   = "34fc21309a8e462cb491a0d7610ea489" # this is the secret
-PROJECT_ID = "brn0ti14jplfqcpojb60"            # this is the project id
+USERNAME   = "SERVICE_ACCOUNT_KEY"       # this is the key
+PASSWORD   = "SERVICE_ACCOUT_SECRET"     # this is the secret
+PROJECT_ID = "PROJECT_ID"                # this is the project id
 
 # url base and endpoint
 API_URL_BASE  = "https://api.disruptive-technologies.com/v2"
